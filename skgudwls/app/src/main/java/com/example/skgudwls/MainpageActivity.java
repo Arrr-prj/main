e833dde5a88dd0354ffdbca34fe2b7da1e204410
qTeTp <k355323@gmail.com>
2023-08-08 11:56:33 +0900

    login&main&list
    끼얏호우

--- skgudwls/app/src/main/java/com/example/skgudwls/MainpageActivity.java
+++ skgudwls/app/src/main/java/com/example/skgudwls/MainpageActivity.java
@@ -2,7 +2,10 @@ package com.example.skgudwls;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainpageActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainpageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+        // 임시 버튼 생성
+        Button imageButton = (Button) findViewById(R.id.button4);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PublicauctionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
