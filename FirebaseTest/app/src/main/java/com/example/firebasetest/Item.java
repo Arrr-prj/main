0d0207eb23ea053e5c6c1d930d4ba87a00b82c04
youngsil <koo1175@naver.com>
2023-08-09 16:14:12 +0900

    start
    start commit

---
+++ FirebaseTest/app/src/main/java/com/example/firebasetest/Item.java
@@ -0,0 +1,69 @@
+package com.example.firebasetest;
+
+public class Item {
+    private String imageUrl;
+    private String id;
+    private Integer price;
+    private String category;
+    private String info;
+    private String seller;
+
+    public Item(){
+
+    }
+    public Item(String imageUrl, String id, Integer price, String category, String info){
+        this.id = id;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.category = category;
+        this.info = info;
+        this.seller = getSeller();
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String profile) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
